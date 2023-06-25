aa11d729bd56a4a70c91915975b723bdb28dcf65
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-26 00:17:58 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,40 @@
-import React, {useState, useEffect} from "react";
-import items from "./data";
+import React, { useState, useEffect } from "react";
+// import items from "./data";
 import "./App.css";
 
-function App() {
-  return (
-    <section id="me">
-      <BookListing />
-    </section>
-  );
+    function App() {
+      // const url = "https://api.github.com/users";
+      const url = "https://catfact.ninja/fact";
+      const [users, setUsers] = useState([])
+      const getUsers = async () =>{
+        const response = await fetch(url)
+        const users = await response.json()
+        setUsers(users)
+        console.log(users);
+      }
+useEffect(() =>{
+  getUsers()
+    },[])
+
+// const newppl = users.map((user) =>{
+//     const {id, fact} = user
+//     return <li key={id}>
+//       <p>{fact}</p>
+//     </li>
+//    })
+function name(params) {
+  console.log('hi');
 }
- const url = "https://api.github.com/users";
 
-const BookListing = () => {
-  const [users, setUsers] = useState([])
- async function getUsers() {
-    const response = await fetch(url)
-    const users = await response.json()
-    console.log(users);
-    setUsers(users)
-  }
-
-useEffect(()=>{
- getUsers()
-}, [])
-
-  const [people, setPeople] =useState(items);
-  const [value, setValue] = useState(0);
-
-  function handleClick(id) {
-    const newArray = people.filter((person) => person.id !== id);
-    setPeople(newArray);
-  }
-
-
-  useEffect(() =>{
-    document.title = `New Messages:${value >= 1 ? value : 0}`
-  }, [value])
-  function handleIncrement() {
-  
-    setTimeout(() => {
-      setValue(prevValue => prevValue + 1);
-    }, 2000);
-  }
-  console.log("i run first");
-
-  const books = people.map((item) => {
-    return<Book key={item.id} {...item} handleClick={handleClick} />;
-  });
-  
-  return (
-    <section className="books-section">
-      {/* <ul>
-        {users.forEach((user) => {
-          const { id, avatar_url} = user;
-        return ( <li key={id}>
-          console.log(user);
-          </li>)
-        })}
-      </ul> */}
-      {books}
-      <h1>{value}</h1>
-      <div className="btn-section">
-        <button onClick={handleIncrement}>Click Me</button>
-      </div>
-    </section>
-  );
-};
-
-function Book({id, image, title, author, price, description, publicationYear, genre, handleClick}) {
-  return (
-    <article>
-      <h1>Genre: {genre}</h1>
-      <img src={image} alt="" />
-      <h2>{title}</h2>
-      <h5>{description}</h5>
-      <h1>{price}</h1>
-      <h6>{author}</h6>
-      <p> Publication Year:{publicationYear}</p>
-      <button onClick={() => handleClick(id)}>delete</button>
-    </article>
-  );
+  name()
+ return (
+   <>
+   <h3>github users</h3>
+   <ul>
+  ;o
+   </ul>
+   </>
+ );
 }
 
 export default App;
--- src/check.jsx
+++ src/check.jsx
@@ -1,68 +1,68 @@
-import React from "react";
-import items from "./data";
-import "./App.css";
+// import React from "react";
+// import items from "./data";
+// import "./App.css";
 
-function App() {
-  return (
-    <section>
-      <BookListing />
-    </section>
-  );
-}
+// function App() {
+//   return (
+//     <section>
+//       <BookListing />
+//     </section>
+//   );
+// }
 
-const BookListing = () => {
-  const [people, setPeople] = React.useState(items);
-  const [isText, setIsText] = React.useState(true);
+// const BookListing = () => {
+//   const [people, setPeople] = React.useState(items);
+//   const [isText, setIsText] = React.useState(true);
 
-  function handleClick(id) {
-    const newArray = people.filter((person) => person.id !== id);
-    // console.log(newArray);
-    setPeople(newArray);
-  }
+//   function handleClick(id) {
+//     const newArray = people.filter((person) => person.id !== id);
+//     // console.log(newArray);
+//     setPeople(newArray);
+//   }
 
-  function changeText() {
-    setIsText((preIsText) => !preIsText);
-  }
+//   function changeText() {
+//     setIsText((preIsText) => !preIsText);
+//   }
 
-  const books = people.map((item) => {
-    <Book key={item.id} {...item} handleClick={handleClick} />;
-  });
+//   const books = people.map((item) => {
+//     <Book key={item.id} {...item} handleClick={handleClick} />;
+//   });
 
-  return (
-    <section className="books-section">
-      {books}
-      <h1>{isText ? "Hello Nigerians awesome react" : "Awseome World"}</h1>
-      <button type="text" onClick={changeText}>
-        Change text
-      </button>
-    </section>
-  );
-};
+//   return (
+//     <section className="books-section">
+//       {books}
+//       <h1>{isText ? "Hello Nigerians awesome react" : "Awseome World"}</h1>
+//       <button type="text" onClick={changeText}>
+//         Change text
+//       </button>
+//     </section>
+//   );
+// };
 
-function Book(props) {
-  const {
-    image,
-    title,
-    author,
-    price,
-    description,
-    publicationYear,
-    genre,
-    handleClick,
-  } = props;
-//   console.log(props);
-  return (
-    <article>
-      <h1>Genre: {genre}</h1>
-      <img src={image} alt="" />
-      <h2>{title}</h2>
-      <h5>{description}</h5>
-      <h1>{price}</h1>
-      <h6>{author}</h6>
-      <p> Publication Year:{publicationYear}</p>
-      <button onClick={() => handleClick(id)} className="btn">delete</button>
-    </article>
-  );
-}
+// function Book(props) {
+//   const {
+//     image,
+//     title,
+//     author,
+//     price,
+//     description,
+//     publicationYear,
+//     genre,
+//     handleClick,
+//   } = props;
+// //   console.log(props);
+//   return (
+//     <article>
+//       <h1>Genre: {genre}</h1>
+//       <img src={image} alt="" />
+//       <h2>{title}</h2>
+//       <h5>{description}</h5>
+//       <h1>{price}</h1>
+//       <h6>{author}</h6>
+//       <p> Publication Year:{publicationYear}</p>
+//       <button onClick={() => handleClick(id)} >delete</button>
+//     </article>
+//   );
+// }
 
-export default App;
+// export default App;
