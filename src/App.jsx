3ef71195d6dab3a6d51e432c773817104ed4b2a6
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-26 06:04:26 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import items from "./data";
+import items from "./data";
 import "./App.css";
 
-    function App() {
-      // const url = "https://api.github.com/users";
-      const url = "https://catfact.ninja/fact";
-      const [users, setUsers] = useState([])
-      const getUsers = async () =>{
-        const response = await fetch(url)
-        const users = await response.json()
-        setUsers(users)
-        console.log(users);
-      }
-useEffect(() =>{
-  getUsers()
-    },[])
-
-// const newppl = users.map((user) =>{
-//     const {id, fact} = user
-//     return <li key={id}>
-//       <p>{fact}</p>
-//     </li>
-//    })
-function name(params) {
-  console.log('hi');
-}
-
-  name()
- return (
-   <>
-   <h3>github users</h3>
-   <ul>
-  ;o
-   </ul>
-   </>
- );
-}
-
-export default App;
+  
