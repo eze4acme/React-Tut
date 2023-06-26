eb969edd2f4dfc212b24d49200f3b897bb68c274
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-26 14:12:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
-import React, { useState, useEffect } from "react";
-import items from "./data";
+// import React, { useState, useEffect } from "react";
+// import items from "./data";
 import "./App.css";
 
   
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState(''); // Initialize data as null
+
+  useEffect(() => {
+    fetch("https://api.github.com/users")
+      .then((response) => response.json())
+      .then((result) => setData(result))
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>; // Show a loading message until data is fetched
+  }
+
+  return (
+    <div>
+       <h2>Github Users</h2>
+       <div className="users">
+        {data.map((user) =>{
+            const {id, avatar_url, html_url, login} = user
+            return <li key={id}>
+                <img src={avatar_url} alt="" />
+                <div className="text-section">
+                    <h4>{login}</h4>
+                    <a href={html_url}>Profile</a>
+                </div>
+            </li>
+        })}
+       </div>
+    </div>
+  );
+}
+
+export default App;
--- src/check.jsx
+++ src/check.jsx
@@ -1,68 +1,26 @@
-// import React from "react";
-// import items from "./data";
-// import "./App.css";
 
-// function App() {
-//   return (
-//     <section>
-//       <BookListing />
-//     </section>
-//   );
-// }
+import React, { useEffect, useState } from "react";
+function App() {
+  const [data, setData] = useState([]);
 
-// const BookListing = () => {
-//   const [people, setPeople] = React.useState(items);
-//   const [isText, setIsText] = React.useState(true);
+  useEffect(() => {
+    // This function will be executed after the component has rendered
+    // Example: Fetching data from an API
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then((response) => response.json())
+      .then((result) => setData(result))
+      .catch((error) => console.error(error));
+  }, []); 
+  return (
+    <div>
+      {/* Render the fetched data */}
+      {data.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </div>
+  );
+}
 
-//   function handleClick(id) {
-//     const newArray = people.filter((person) => person.id !== id);
-//     // console.log(newArray);
-//     setPeople(newArray);
-//   }
+export default App
 
-//   function changeText() {
-//     setIsText((preIsText) => !preIsText);
-//   }
 
-//   const books = people.map((item) => {
-//     <Book key={item.id} {...item} handleClick={handleClick} />;
-//   });
-
-//   return (
-//     <section className="books-section">
-//       {books}
-//       <h1>{isText ? "Hello Nigerians awesome react" : "Awseome World"}</h1>
-//       <button type="text" onClick={changeText}>
-//         Change text
-//       </button>
-//     </section>
-//   );
-// };
-
-// function Book(props) {
-//   const {
-//     image,
-//     title,
-//     author,
-//     price,
-//     description,
-//     publicationYear,
-//     genre,
-//     handleClick,
-//   } = props;
-// //   console.log(props);
-//   return (
-//     <article>
-//       <h1>Genre: {genre}</h1>
-//       <img src={image} alt="" />
-//       <h2>{title}</h2>
-//       <h5>{description}</h5>
-//       <h1>{price}</h1>
-//       <h6>{author}</h6>
-//       <p> Publication Year:{publicationYear}</p>
-//       <button onClick={() => handleClick(id)} >delete</button>
-//     </article>
-//   );
-// }
-
-// export default App;
